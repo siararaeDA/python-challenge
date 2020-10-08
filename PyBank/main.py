c101ea767fd318064fb0cccbcdd69f04c653bc05
Siara Leininger <leinings@oregonstate.edu>
2020-10-08 13:46:24 -0700

    Outlined instructions for PyBank.

---
+++ PyBank/main.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+# Find the total number of months in the dataset
+
+# Find the net total of Profit/Losses (column 2) over the entire period.
+
+# Find the average of the changes in Profit/Losses over the entire period.
+
+# Find the greatest increase in profits (date and amount) over the entire period.
+
+# Find the greatest decrease in losses (date and amount) over the entire period.
+
+# Example output:
+#
+# Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $38382578
+# Average  Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
+# Print analysis to the terminal and to a text file
