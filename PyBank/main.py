caac517dd3fadf6238408102c67054ce816e4e88
Siara Leininger <leinings@oregonstate.edu>
2020-10-12 13:06:11 -0700

    Fixed pathing.

--- PyBank/main.py
+++ PyBank/main.py
@@ -66,7 +66,8 @@ monthsList = []
 profitsLossesList = []
 
 # Read in the data
-csvpath = os.path.join(os.path.dirname(__file__), 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
+# Add this to the path to run from a different folder: os.path.dirname(__file__), 
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -103,8 +104,9 @@ line7 = str(f"Greatest Decrease in Profits: {greatestDecreaseProfits}\n")
 
 outputLines = [line1, line2, line3, line4, line5, line6, line7]
 
-filename = 'Results.txt'
-with open(filename, 'w') as file:
+filename = 'PyBankResults.txt'
+path = 'analysis/'
+with open(os.path.join(path, filename), 'w') as file:
     for line in outputLines:
         print(line)
         file.write(line)
